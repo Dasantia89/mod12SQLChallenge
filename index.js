bc6e5e0999c8292873d79a4b18f8fadd6e5fa3fc
Dasantia89 <143134358+Dasantia89@users.noreply.github.com>
2023-12-01 21:45:26 -0500

    Converted mysql to promise version

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Require needed npm modules
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 require('dotenv').config();
 var inquirer = require('inquirer');
 const { printTable } = require('console-table-printer');
@@ -9,8 +9,8 @@ const questions = [
     {
         type: 'list',
         name: 'choice',
-        message: 'What shape should the logo have?',
-        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Quit']
+        message: 'What do you want to do?',
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role']
     }
 ];
 
@@ -21,11 +21,14 @@ async function init() {
     var isDisplayed = false;
     const answers = await inquirer.prompt(questions);
 
-    const db = mysql.createConnection({
+    const db = mysql.createPool({
         host: 'localhost',
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
+        database: process.env.DB_NAME,
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0
     });
 
 
@@ -38,40 +41,46 @@ async function init() {
     } else if (answers.choice == 'View all employees') {
         query = 'SELECT * FROM employees;';
         isDisplayed = true;
-    } else if (answers.choice == 'Add a department') {
-        const answers2 = await inquirer.prompt([{ type: 'input', name: 'dept', message: 'What is the name of the department you want to add?' }])
+    }
+    else if (answers.choice == 'Add a department') {
+        const answers2 = await inquirer.prompt([{ type: 'input', name: 'dept', message: 'What is the name of the department you want to add?' }]);
         query = `INSERT INTO departments (name) VALUES ("${answers2.dept}");`
-        console.log('Department added.')
+        var x = await db.query(query);
+        console.log('Department added.');
         db.end();
         return;
-    } else if (answers.choice == 'Add a role') {
-        var depts;
-        db.query('SELECT name FROM departments;', function (err, results) {
-            depts = results.map((dept) => dept.name)
-        })
+    } 
+    else if (answers.choice == 'Add a role') {
+        var [results] = await db.query('SELECT name FROM departments;');
+        var depts = results.map((department)=>department.name);
         const answers2 = await inquirer.prompt([{ type: 'input', name: 'name', message: 'What is the name of the role?' }]);
         const answers3 = await inquirer.prompt([{ type: 'input', name: 'salary', message: 'What is the salary of the role?' }]);
-        const answers4 = await inquirer.prompt([{ type: 'rawlist', name: 'dept', message: 'What department does the role belong to?', choices: depts }])
+        const answers4 = await inquirer.prompt([{ type: 'list', name: 'dept', message: 'What department does the role belong to?', choices: depts }])
 
         var deptQuery = `SELECT id FROM departments WHERE name = "${answers4.dept}";`;
-        db.query(deptQuery, function (err, results) {
-            query = `INSERT INTO roles (title, salary, department_id) VALUES ("${answers2.name}",${answers3.salary},${results[0].id});`;
-            db.query(query, function (err, results, fields) {
-                console.log('Role added.')
-            });
-        })
+        var [results] = await db.query(deptQuery);
+
+
+        query = `INSERT INTO roles (title, salary, department_id) VALUES ("${answers2.name}",${answers3.salary},${results[0].id});`;
+        await db.query(query);
+        console.log('Role added.')
         db.end();
         return;
-    }
-    // For simple select statements only
+    } 
+  
+
+
+    
+
     if (isDisplayed) {
-        db.query(query, function (err, results) {
-            printTable(results);
-        }
-        )
+      var results =  await db.query(query)
+      printTable(results[0]);
         db.end();
         return;
     }
 }
-    // Function call to initialize app
+
+
+
+// Function call to initialize app
 init();
